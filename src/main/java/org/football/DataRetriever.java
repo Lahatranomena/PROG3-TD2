1a3e77efc7418c1f8e909d254cabd868a7781492
Lahatranomena <hei.lahatra.3@gmail.com>
2025-12-19 22:23:38 +0300

    add methods

--- src/main/java/org/football/DataRetriever.java
+++ src/main/java/org/football/DataRetriever.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DataRetriever {
 
@@ -28,12 +30,12 @@ public class DataRetriever {
             try (ResultSet resultSet = statement.executeQuery()) {
                 Team team = null;
                 while (resultSet.next()) {
-                        team = new Team(
-                                resultSet.getInt("team_id"),
-                                resultSet.getString("team_name"),
-                                ContinentEnum.valueOf(resultSet.getString("continent"))
-                        );
-                        team.setPlayers(players);
+                    team = new Team(
+                            resultSet.getInt("team_id"),
+                            resultSet.getString("team_name"),
+                            ContinentEnum.valueOf(resultSet.getString("continent"))
+                    );
+                    team.setPlayers(players);
                     {
                         Player player = new Player();
                         player.setId(resultSet.getInt("player_id"));
@@ -70,15 +72,15 @@ public class DataRetriever {
 
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
-                        Player player = new Player();
-                        player.setId(resultSet.getInt("player_id"));
-                        player.setName(resultSet.getString("player_name"));
-                        player.setAge(resultSet.getInt("age"));
-                        player.setPosition(
-                                PlayerPositionEnum.valueOf(resultSet.getString("positions"))
-                        );
+                    Player player = new Player();
+                    player.setId(resultSet.getInt("player_id"));
+                    player.setName(resultSet.getString("player_name"));
+                    player.setAge(resultSet.getInt("age"));
+                    player.setPosition(
+                            PlayerPositionEnum.valueOf(resultSet.getString("positions"))
+                    );
 
-                        players.add(player);
+                    players.add(player);
                 }
             }
             return players;
@@ -87,21 +89,148 @@ public class DataRetriever {
         }
     }
 
-    List<Player> createPlayers(List<Player> newPlayers)  throws SQLException {
-        throw new RuntimeException();
+
+    List<Player> createPlayers(List<Player> newPlayers) {
+
+        String insertQuery = """
+                   INSERT INTO player player.id AS player_id, player.name AS player_name,\s
+                   player.age AS age, player.position AS position VALUES (?, ?, ?, ?)
+               \s""";
+
+        try (Connection conn = connection.getDBConnection()) {
+            conn.setAutoCommit(false);
+            Set<String> names = new HashSet<>();
+            for (Player player : newPlayers) {
+                if (!names.add(player.getName())) {
+                    throw new RuntimeException(
+                            "Player " + player.getName() + " is duplicated in the list."
+                    );
+                }
+            }
+            try (PreparedStatement statement = conn.prepareStatement(insertQuery)) {
+                for (Player player : newPlayers) {
+                    statement.setInt(1, player.getId());
+                    statement.setString(2, player.getName());
+                    statement.setInt(3, player.getAge());
+                    statement.setString(4, player.getPosition().name());
+                    statement.executeUpdate();
+                }
+            }
+            conn.commit();
+            System.out.println("Success");
+            return newPlayers;
+        } catch (Exception e) {
+            throw new RuntimeException("Transaction cancel : ", e);
+        }
     }
 
-    Team saveTeam(Team teamToSave) throws SQLException {
-        throw new RuntimeException();
+//    Team saveTeam(Team teamToSave) throws SQLException {
+//
+//        String sql = """
+//                SELECT team.id, team.name FROM team where id = ?;
+//                """;
+//
+//        try (PreparedStatement statement = connection.getDBConnection().prepareStatement(sql)) {
+//            statement.setInt(1, teamToSave.getId());
+//        }
+//        throw new RuntimeException();
+//    }
+
+    List<Team> findTeamsByPlayerName(String playerName) throws SQLException {
+
+        List<Team> teams = new ArrayList<>();
+        Player player = new Player();
+
+        StringBuilder findTeamsByPlayerNameQuery = new StringBuilder("""
+                SELECT team.id AS team_id, team.name AS team_name, team.continent AS continent, 
+                player.name AS player_name FROM team LEFT JOIN player ON player.id_team = team.id
+                WHERE 1 = 1
+                """);
+
+        List<Object> parameters = new ArrayList<>();
+
+        if (playerName != null) {
+            findTeamsByPlayerNameQuery.append("and player.name ilike ? ");
+            parameters.add("%" + playerName + "%");
+        }
+        try (Connection conn = connection.getDBConnection();
+             PreparedStatement statement = conn.prepareStatement(findTeamsByPlayerNameQuery.toString())) {
+            for (int i = 0; i < parameters.size(); i++) {
+                statement.setObject(i + 1, parameters.get(i));
+            }
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                if (rs.getInt("team_id") != 0) {
+                    Team team = new Team();
+                    team.setId(rs.getInt("team_id"));
+                    team.setName(rs.getString("team_name"));
+                    team.setContinent(ContinentEnum.valueOf(rs.getString("continent")));
+                    teams.add(team);
+                }
+            }
+
+        }
+        return teams;
     }
 
-    List<Team> findTeamsByPlayerName(String playerName)  throws SQLException {
-        throw new RuntimeException();
-    }
 
-    List<Player> findPlayersByCriteria(String playerName,
-                                       PlayerPositionEnum position, String teamName, ContinentEnum
-                                               continent, int page, int size)   throws SQLException {
-        throw new RuntimeException();
+    List<Player> findPlayersByCriteria(String playerName, PlayerPositionEnum position, String teamName,
+            ContinentEnum continent, int page, int size
+    ) throws SQLException {
+        Team team = new Team();
+
+        List<Player> players = new ArrayList<>();
+        StringBuilder sql = new StringBuilder("""
+                SELECT player.id AS player_id, player.name AS player_name, player.age AS age, player.position AS position,\s
+                team.name AS team FROM  player LEFT JOIN team ON player.id_team = team.id
+                where 1 = 1
+               \s""");
+
+        List<Object> parameters = new ArrayList<>();
+
+        if (playerName != null) {
+            sql.append("and player.name like ? ");
+            parameters.add("%" + playerName + "%");
+        }
+        if (position != null) {
+            sql.append("and player.position = ? ");
+            parameters.add(position);
+        }
+        if (teamName != null) {
+            sql.append("and team.name = ? ");
+            parameters.add(teamName);
+        }
+        if (continent != null) {
+            sql.append("and player.position = ? ");
+            parameters.add(continent);
+        }
+        sql.append("limit ? offset ?");
+
+        int offset = (page - 1) * size;
+        parameters.add(size);
+        parameters.add(offset);
+
+        try (Connection conn = connection.getDBConnection();
+             PreparedStatement statement = conn.prepareStatement(sql.toString())) {
+
+            for (int i = 0; i < parameters.size(); i++) {
+                statement.setObject(i + 1, parameters.get(i));
+            }
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                if (rs.getInt("player_id") != 0) {
+                    Player player = new Player();
+                    player.setId(rs.getInt("player_id"));
+                    player.setName(rs.getString("player_name"));
+                    player.setAge(rs.getInt("age"));
+                    player.setPosition(PlayerPositionEnum.valueOf(rs.getString("position")));
+                    team.setName(rs.getString("team"));
+                    player.setTeam(team);
+                    players.add(player);
+                }
+            }
+        }
+        return players;
     }
 }
+
--- src/main/java/org/football/Main.java
+++ src/main/java/org/football/Main.java
@@ -1,15 +1,32 @@
 package org.football;
 
-
-import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         DBConnection dbc = new DBConnection();
         DataRetriever dr = new DataRetriever();
 
-        System.out.println(dr.findTeamById(1));
-        System.out.println(dr.findPlayers(3,2));
+
+        List<Player> newPlayers = new ArrayList<>();
+        Team team = new Team(6, "ASIATIC", ContinentEnum.ASIA);
+        Player player1 = new Player(5, "Nomena", 19, PlayerPositionEnum.GK, team);
+        Player player2 = new Player(6, "Lahatra", 20, PlayerPositionEnum.DEF, team   );
+
+        try {
+            List<Player> inserted = dr.createPlayers(newPlayers);
+
+            System.out.println("Number of player inserted : " + inserted.size());
+            for (Player p : inserted) {
+                System.out.println("Days created : " + p.getName());
+            }
+
+        } catch (RuntimeException e) {
+            System.out.println("Error during insertion : " + e.getMessage());
+        }
+
+
     }
 }
